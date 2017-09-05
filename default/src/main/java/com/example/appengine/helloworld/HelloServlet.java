2fa6c1e8d693ca83f621a001fa01e3f445a59486
T.V.R.L.PhaniKumar Dadi <pdadi@miraclesoft.com>
2017-09-06 03:20:22 +0530

    Update HelloServlet.java

--- default/src/main/java/com/example/appengine/helloworld/HelloServlet.java
+++ default/src/main/java/com/example/appengine/helloworld/HelloServlet.java
@@ -30,7 +30,7 @@ public class HelloServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     PrintWriter out = resp.getWriter();
-    out.println("Hello, I'm a microservice");
+    out.println("Hello, I'm just testing");
   }
 }
 // [END example]
